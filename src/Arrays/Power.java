44e830e43736425af0201a814bca1ff86813eb1f
Nishant Chaudhary <b0215260@LTB0215260-Mac.local>
2022-04-03 00:09:16 +0530

    added programs

---
+++ src/Arrays/Power.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Power {
+
+    public static void calpow(int[] arr) {
+        int base, power, index = 0, product = 1;
+        int[] prodlist = new int[arr.length];
+        for (int i = 0; i < arr.length - 1; i++) {
+            base = arr[i];
+            power = arr[i + 1];
+            while (power != 0) {
+                product *= base;
+                power--;
+            }
+            prodlist[i] = product;
+            product = 1;
+        }
+        System.out.println(Arrays.toString(prodlist));
+
+        int max = prodlist[0];
+        for (int i = 1; i < prodlist.length - 1; i++) {
+            if (prodlist[i] > max) {
+                max = prodlist[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+
+    }
+
+
+    public static void main(String[] args) {
+        calpow(new int[]{2, 10, 3, 7});
+    }
+}
---
+++ src/Mathematics/DecimalToBinary.java
@@ -0,0 +1,23 @@
+package Mathematics;
+
+import java.util.Arrays;
+
+public class DecimalToBinary {
+    public static void convertDecToBinary(int n) {
+        int[] binary = new int[100];
+        int number, index = 0;
+        while (n > 0) {
+            number = n % 2;
+            binary[index] = number;
+            n = n / 2;
+            index++;
+        }
+        for (int i = index - 1; i >= 0; i--) {
+            System.out.print(binary[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        convertDecToBinary(8);
+    }
+}
