ebef40c5137d6bd964601371f7f4a34a256232c4
Nishant Chaudhary <b0215260@LTB0215260-Mac.local>
2022-05-06 20:17:19 +0530

    updated

--- src/MultiThreading/MultiThreading.java
+++ src/MultiThreading/MultiThreading.java
@@ -1,2 +1,26 @@
-package MultiThreading;public class MultiThreading {
+package MultiThreading;
+
+public class MultiThreading implements Runnable {
+
+
+    private final int threadNumber;
+
+    public MultiThreading(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " from thread number " + threadNumber);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            MultiThreading obj = new MultiThreading(i);
+            Thread objNew= new Thread(obj);
+            objNew.start();
+        }
+    }
 }
