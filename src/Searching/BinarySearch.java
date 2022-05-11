215759e02c8b37cdaf8ba6545bf4e5d04eee7620
Nishant Chaudhary <b0215260@LTB0215260-Mac.local>
2022-05-12 01:03:42 +0530

    binary search

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -1,2 +1,25 @@
-package Searching;public class BinarySearch {
+package Searching;
+
+public class BinarySearch {
+
+    private static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[]{1, 3, 5, 6, 8, 9}, 1));
+    }
 }
