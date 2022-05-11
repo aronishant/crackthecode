b7dfd30170e97e20cc1b6a454f4ee9ba8e75c1bb
Nishant Chaudhary <b0215260@LTB0215260-Mac.local>
2022-05-12 01:08:06 +0530

    deleted

--- src/ArraysIntermediate/ProductOfArrayExceptSelf.java
+++ src/ArraysIntermediate/ProductOfArrayExceptSelf.java
@@ -1,2 +1,28 @@
-package ArraysIntermediate;public class ProductOfArrayExceptSelf {
+package ArraysIntermediate;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    static int[] product(int[] arr) {
+        int[] prod = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            int p = 1;
+            for (int j = 0; j < arr.length; j++) {
+                if (i==j){
+                    continue;
+                }
+                p = p * arr[j];
+            }
+            prod[i] = p;
+
+        }
+        return prod;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(product((new int[]{1, 2, 3, 4}))));
+    }
 }
--- src/oops/Child.java
+++
@@ -1,2 +0,0 @@
-package oops;public class Child {
-}
