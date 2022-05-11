87e34128307024411df241770e3f8938b90c0a4a
Nishant Chaudhary <b0215260@LTB0215260-Mac.local>
2022-05-12 01:04:42 +0530

    reverse a string

--- src/Strings/Reverse.java
+++ src/Strings/Reverse.java
@@ -1,2 +1,29 @@
-package Strings;public class Reverse {
+package Strings;
+
+public class Reverse {
+
+    static void reverse(String str) {
+        char[] chars = str.toCharArray();
+        int i = 0;
+        int j = chars.length - 1;
+        while (i <= j) {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println(chars);
+    }
+
+    static void printReverse(String str){
+        for (int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        reverse("Nishant");
+        printReverse("Nishant");
+    }
 }
